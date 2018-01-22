e0ded6fb9b62880089daa829188485b4ff0a0cdd
Rocky <274935730@qq.com>
2018-01-22 15:07:50 +0800

    add ElasticsearchClient

--- papa_office/papa_office/es/ElasticsearchClient.py
+++ papa_office/papa_office/es/ElasticsearchClient.py
@@ -1,8 +1,35 @@
 # -*- coding:utf-8 -*-
 from elasticsearch import Elasticsearch
 from papa_office.conf.GlobleConfig import *
-es = Elasticsearch(ELASTICSEARCH_URL)
-# data = es.get(index="judicial", doc_type="judicialDishonesty", id='1gL7w14BmAAgcahOcziX')['_source']
-# print data
-res = es.search(index="judicial",doc_type='judicialDishonesty', body={"query": {"match_all": {}}})#"outputValue":'金融借款合同纠纷'
-print res
+
+class EladsticsearchClient(object):
+
+    def __init__(self):
+        self.client = Elasticsearch(ELASTICSEARCH_URL)
+
+    def search(self,index,docType,body):
+        return self.client.search(index,docType,body)
+
+class Query(object):
+    @staticmethod
+    def getMatchQuery(simpleConditions):
+        query = {}
+        matchCondition = Query.Match(simpleConditions)
+        query['query'] = matchCondition.match
+        return query
+
+    class Match(object):
+        def __init__(self,match):
+            self.match = {}
+            self.match['match'] = match
+
+####################################example############################################
+# clt = EladsticsearchClient()
+#
+# d = {}
+# d['outputValue'] = u'金融借款合同纠纷'
+# m = Query.getMatchQuery(d)
+# print m
+# res = clt.search(index="judicial",docType='judicialDishonesty', body=m)
+# print res#['_source']
+####################################example############################################
