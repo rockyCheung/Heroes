3e4815c3ee6c7db83068d07ee6bab626bbfa2e1d
rockyCheung <274935730@qq.com>
2019-05-07 17:52:03 +0800

    优化登录首页显示信息问题

--- test/testpybus.py
+++
@@ -1,6 +0,0 @@
-from pydbus import SessionBus
-
-bus = SessionBus()
-notifications = bus.get('.Notifications')
-
-notifications.Notify('test', 0, 'dialog-information', "Hello World!", "pydbus works :)", [], {}, 5000)
